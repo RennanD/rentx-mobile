bab200d78df73c45172060e2f23c1b968953ff75
rennand <rplaykl@gmail.com>
2021-06-07 23:22:22 -0300

    feat: create car informations

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,7 +3,20 @@ import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
 import { ImagesSlider } from '../../components/ImagesSlider';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  CarName,
+  Rent,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export function CarDetails(): JSX.Element {
   const images = [
@@ -26,6 +39,26 @@ export function CarDetails(): JSX.Element {
       <CarImages>
         <ImagesSlider imagesUrl={images} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>AUDI</Brand>
+            <CarName>RS 5 Coupé</CarName>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 120</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,6 +1,7 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -22,4 +23,80 @@ export const Header = styled.View`
 
 export const CarImages = styled.View`
   margin-top: ${getStatusBarHeight() + 32}px;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center',
+  },
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 38px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    color: ${theme.colors.text_detail};
+  `}
+`;
+
+export const CarName = styled.Text`
+  font-size: ${RFValue(25)}px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    color: ${theme.colors.title};
+  `}
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-size: ${RFValue(10)}px;
+
+  text-transform: uppercase;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    color: ${theme.colors.text_detail};
+  `}
+`;
+
+export const Price = styled.Text`
+  font-size: ${RFValue(25)}px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.primary_500};
+    color: ${theme.colors.main};
+  `}
+`;
+
+export const About = styled.Text`
+  font-size: ${RFValue(15)}px;
+
+  text-align: justify;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.secondary_400};
+    color: ${theme.colors.text};
+  `}
+
+  margin-top: 23px;
 `;
