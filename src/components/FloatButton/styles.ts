0ab0de8f0f3b0b2a36eac3c7d741c2eb99ea039e
RennanD <rplaykl@gmail.com>
2021-06-30 23:22:55 -0300

    feat: animte about car scroll

--- src/components/FloatButton/styles.ts
+++ src/components/FloatButton/styles.ts
@@ -3,13 +3,13 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled(RectButton)`
-  height: ${RFValue(60)}px;
-  width: ${RFValue(60)}px;
+  height: ${RFValue(50)}px;
+  width: ${RFValue(50)}px;
 
   align-items: center;
   justify-content: center;
 
-  border-radius: ${RFValue(30)}px;
+  border-radius: ${RFValue(25)}px;
 
   position: absolute;
 
