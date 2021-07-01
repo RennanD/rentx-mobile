570bb5f0ed55771208ffb67a244ff76e91ad9ff7
RennanD <rennan.douglas@objetivopi.com.br>
2021-07-01 13:30:31 -0300

    feat: create float button animation

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -1,24 +1,91 @@
 import React from 'react';
-import { RectButtonProps } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+
+import {
+  RectButtonProps,
+  RectButton,
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
 
 import { useTheme } from 'styled-components';
 
 import { Ionicons } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 type FloatButtonProps = RectButtonProps;
+
+type ContextPostion = {
+  positionInX: number;
+  positionInY: number;
+};
+
+const AnimatedButton = Animated.createAnimatedComponent(RectButton);
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 13,
+    right: 22,
+  },
+  button: {
+    height: RFValue(50),
+    width: RFValue(50),
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: RFValue(25),
+  },
+});
 
 export function FloatButton({ ...rest }: FloatButtonProps): JSX.Element {
   const theme = useTheme();
 
+  const positionInX = useSharedValue(0);
+  const positionInY = useSharedValue(0);
+
+  const animatedButtonStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: positionInX.value },
+      { translateY: positionInY.value },
+    ],
+  }));
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, context: ContextPostion) {
+      context.positionInX = positionInX.value;
+      context.positionInY = positionInY.value;
+    },
+    onActive(event, context: ContextPostion) {
+      positionInX.value = context.positionInX + event.translationX;
+      positionInY.value = context.positionInY + event.translationY;
+    },
+    onEnd() {
+      positionInX.value = withSpring(0);
+      positionInY.value = withSpring(0);
+    },
+  });
+
   return (
-    <Container {...rest}>
-      <Ionicons
-        name="ios-car-sport"
-        size={32}
-        color={theme.colors.background_secondary}
-      />
-    </Container>
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View style={[animatedButtonStyle, styles.container]}>
+        <AnimatedButton
+          style={[styles.button, { backgroundColor: theme.colors.main }]}
+          {...rest}
+        >
+          <Ionicons
+            name="ios-car-sport"
+            size={32}
+            color={theme.colors.background_secondary}
+          />
+        </AnimatedButton>
+      </Animated.View>
+    </PanGestureHandler>
   );
 }
